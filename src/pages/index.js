7152355f7b6482c3477c7ab5c7f0e4b41053fa33
Adin Castro <acastro@agrobiotek.com>
2020-01-06 19:17:24 -0600

    change case sensitivity in folders names

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Landing from '../components/sections/landing'
-import About from '../components/sections/about'
-import Tech from '../components/sections/tech'
-import Works from '../components/sections/works'
-import Contact from '../components/sections/contact'
+import Landing from '../components/content-sections/landing'
+import About from '../components/content-sections/about'
+import Tech from '../components/content-sections/tech'
+import Works from '../components/content-sections/works'
+import Contact from '../components/content-sections/contact'
 
 
 const IndexPage = () => (
