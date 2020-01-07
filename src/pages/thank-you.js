277bf7ef9cd05d36047c0cbc728368b8f960acba
Adin Castro <acastro@agrobiotek.com>
2020-01-06 19:20:50 -0600

    change folder root in thank-you page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ThankYou from '../components/sections/thank-you'
+import ThankYou from '../components/content-sections/thank-you'
 
 const ThankU = () => {
   return (
