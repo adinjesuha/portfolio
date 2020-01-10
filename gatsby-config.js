99bcaf6dd8e3667333fae535d84aeb7f010c97c9
Adin Castro <acastro@agrobiotek.com>
2020-01-10 12:10:47 -0600

    change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `adinjesuha-portfolio`,
+        short_name: `adinjesuha`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FF2848`,
+        theme_color: `#FF2848`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`,
       },
